cfc4a9757753f4d8645d520eb97391e04d367403
Renato Mefi <gh@mefi.in>
2019-05-06 19:06:41 +0200

    Automate init for easier bootstrap

--- init.sh
+++ init.sh
@@ -6,15 +6,17 @@ set -e
 # install initial packages
 sudo dnf install -y vim ansible git zsh libselinux-python python3-dnf
 
-ansible-playbook -i ./hosts.dist -K ./main.yml
+# clone dotfiles, if this doesn't work just clone it in ~/Workstation/dotfiles manually
+mkdir -p ~/Workstation
+read -p "Enter your Github Token (https://github.com/settings/tokens/new): "  TOKEN
 
-# Install and update vundle
-# This should probably be moved to ansible
-mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+test -d ~/Workstation/dotfiles \
+    || git clone --recursive "https://${USER}:${TOKEN}@github.com/${USER}/dotfiles.git" ~/Workstation/dotfiles
+test -d ~/Workstation/my-fedora-workstation \
+    || git clone "https://${USER}:${TOKEN}@github.com/${USER}/my-fedora-workstation.git" ~/Workstation/my-fedora-workstation
 
-# Install tpm and plugins
-# need to automate plugin install
-mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# If .ssh doesn't exist yet, copy it from the dotfiles so we have future permissions to clone projects
+test -d ~/.ssh || cp -R ~/Workstation/dotfiles/ssh ~/.ssh
+
+cd ~/Workstation/my-fedora-workstation \
+    && ansible-playbook -i ./hosts.dist -K ./main.yml
